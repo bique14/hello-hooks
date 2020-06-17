6d3feb41435e50e5783d96dbe4593d43a24c5fc9
bbibbibiq <peerasorn14@gmail.com>
2020-06-17 16:39:37 +0700

    Updates

---
+++ precache-manifest.33fb0ab6b47d5b98be16f27aed01df97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd9f800a141c7c4d905ec1d33b00195",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5337d5c88ed3537ea2a6",
+    "url": "/static/css/main.b227910d.chunk.css"
+  },
+  {
+    "revision": "d0ae2fe2a03c988e11a1",
+    "url": "/static/js/2.94bed068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94bed068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5337d5c88ed3537ea2a6",
+    "url": "/static/js/main.5ada2bcf.chunk.js"
+  },
+  {
+    "revision": "e0585674084ee39feddb",
+    "url": "/static/js/runtime-main.bbc62a06.js"
+  }
+]);
